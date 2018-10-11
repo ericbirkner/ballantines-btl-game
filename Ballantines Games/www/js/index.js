6be24aa2f64d8e4c81b72f9774afd5ff6571711a
Eric Birkner <ericbirkner@gmail.com>
2018-10-11 15:46:00 -0300

    primer commit

---
+++ Ballantines Games/www/js/index.js
@@ -0,0 +1,295 @@
+var db_name="tombola_virtual.db";
+//var db_name="tombola";
+// Wait for Cordova to load
+//toekn para horus
+var logOb;
+//var token = "bc276116-da84-079f-01ca-dbaf91bf77d2";
+var token = "e62b90a3-7ecf-b747-f6eb-f11079fcbacd";
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+var currentRow;
+// Populate the database
+//
+function populateDB(tx) {
+	tx.executeSql('CREATE TABLE IF NOT EXISTS premios (id INTEGER PRIMARY KEY AUTOINCREMENT, name, number)');
+	
+	//creo las tablas de configuracion
+	var sql = "SELECT * FROM premios where name like ('%parlante%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length==0){
+			console.log('no hay parlante');
+			tx.executeSql('INSERT INTO premios (name,number) VALUES ("parlante", "0")');
+		}
+
+	});
+
+
+	var sql = "SELECT * FROM premios where name like ('%finest%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length==0){
+			console.log('parlante');
+			tx.executeSql('INSERT INTO premios (name,number) VALUES ("finest", "0")');
+		}
+
+	});
+
+	var sql = "SELECT * FROM premios where name like ('%audifono%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length==0){
+			console.log('audifono');
+			tx.executeSql('INSERT INTO premios (name,number) VALUES ("audifonos", "0")');
+		}
+
+	});
+}
+
+function creaTablaRegistros(tx) {
+	
+	tx.executeSql('CREATE TABLE IF NOT EXISTS registros (id INTEGER PRIMARY KEY AUTOINCREMENT, firstName,lastName,rut,email,birthday, recibe_info)');
+	
+	//inserto los premios
+	/*
+	tx.executeSql('INSERT INTO premios (id,name,number) VALUES (1,"parlante",100);');
+	tx.executeSql('INSERT INTO premios (id,name,number) VALUES (2,"parlantes",80);');
+	tx.executeSql('INSERT INTO premios (id,name,number) VALUES (3,"audifonos",20);');			  
+	*/
+}
+
+// Query the database
+//
+function queryDB(tx) {
+	tx.executeSql('SELECT * FROM premios', [], querySuccess, errorCB);
+}
+
+function searchQueryDB(tx) {
+	tx.executeSql("SELECT * FROM premios where name like ('%"+ document.getElementById("txtName").value + "%')",
+			[], querySuccess, errorCB);
+}
+// Query the success callback
+//
+function querySuccess(tx, results) {
+	var tblText='<table id="t01" border="1"><tr><th>ID</th> <th>Premio</th> <th>cantidad</th><th>editar</th></tr>';
+	var len = results.rows.length;
+	for (var i = 0; i < len; i++) {
+		var tmpArgs=results.rows.item(i).id + ",'" + results.rows.item(i).name
+				+ "','" + results.rows.item(i).number+"'";
+		tblText +='<tr onclick="goPopup('+ tmpArgs + ');"><td>' + results.rows.item(i).id +'</td><td>'
+				+ results.rows.item(i).name +'</td><td>' + results.rows.item(i).number +'</td><td align="center"><img src="img/edit.png"></td></tr>';
+	}
+	tblText +="</table>";
+	//document.getElementById("").innerHTML =tblText;
+	$('#tblDiv').html(tblText);
+}
+
+//Delete query
+function deleteRow(tx) {
+  tx.executeSql('DELETE FROM premios WHERE id = ' + currentRow, [], queryDB, errorCB);
+}
+
+// Transaction error callback
+//
+function errorCB(err) {
+	//console.log("Error processing SQL: "+err.code);
+}
+
+// Transaction success callback
+//
+function successCB() {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(queryDB, errorCB);
+}
+
+ // Cordova is ready
+//
+function onDeviceReady() {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(populateDB, errorCB, successCB);
+	db.transaction(creaTablaRegistros, errorCB);
+	console.log(cordova.file);
+	
+	/*
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+		//alert(fs.root.name);
+		console.log('file system open: ' + fs.name);
+		fs.root.getFile("registros.txt", { create: true, exclusive: false }, function (fileEntry) {
+
+			console.log("fileEntry is file?" + fileEntry.isFile.toString());
+			// fileEntry.name == 'someFile.txt'
+			// fileEntry.fullPath == '/someFile.txt'
+			writeFile(fileEntry, null);
+
+		});
+
+	});
+	*/
+	
+}
+
+function writeFile(fileEntry, dataObj, isAppend) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+            console.log("Successful file read...");
+            readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+            console.log("Failed file read: " + e.toString());
+        };
+
+        // If we are appending data to file, go to the end of the file.
+        if (isAppend) {
+            try {
+                fileWriter.seek(fileWriter.length);
+            }
+            catch (e) {
+                console.log("file doesn't exist!");
+            }
+        }
+        fileWriter.write(dataObj);
+    });
+}
+
+
+function readFile(fileEntry) {
+
+    fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            console.log("Successful file read: " + this.result);
+			//alert(fileEntry.fullPath);
+            ///displayFileData(fileEntry.fullPath + ": " + this.result);
+        };
+
+        reader.readAsText(file);
+
+    });
+}
+
+
+
+//Insert query
+//
+function insertDB(tx) {
+	var sql = 'INSERT INTO premios (name,number) VALUES ("' +document.getElementById("txtName").value
+			+'","'+document.getElementById("txtNumber").value+'")';
+	console.log(sql);
+	document.getElementById('form').reset();
+	tx.executeSql(sql);
+}
+
+function goInsert() {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(insertDB, errorCB, successCB);
+}
+
+function goSearch() {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(searchQueryDB, errorCB);
+}
+
+function goDelete() {
+	 var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	 db.transaction(deleteRow, errorCB);
+	 document.getElementById('qrpopup').style.display='none';
+}
+
+//Show the popup after tapping a row in table
+//
+function goPopup(row,rowname,rownum) {
+	currentRow=row;
+	document.getElementById("qrpopup").style.display="block";
+	document.getElementById("editNameBox").value = rowname;
+	document.getElementById("editNumberBox").value = rownum;
+}
+
+function editRow(tx) {
+	tx.executeSql('UPDATE premios SET name ="'+document.getElementById("editNameBox").value+
+			'", number= "'+document.getElementById("editNumberBox").value+ '" WHERE id = '
+			+ currentRow, [], queryDB, errorCB);
+}
+function goEdit() {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(editRow, errorCB);
+	document.getElementById('qrpopup').style.display='none';
+}
+
+
+
+function goRegistro() {
+	
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(function(tx){
+	var firstName 	= $( "#firstName" ).val();
+	var lastName 	= $( "#lastName" ).val();
+	var recibe_info	= $( "#recibe_info" ).val();
+	var lastName 	= $( "#lastName" ).val();
+	var email 		= $( "#email" ).val();
+	//var birthday 	= $( "#birthday" ).val();
+	var birthday 	= $("#ano").val()+'-'+$("#mes").val()+'-'+$("#dia").val();	
+	var rut 		= $( "#rut" ).val();
+	var sql = 'INSERT INTO registros (firstName, lastName,rut,email,birthday, recibe_info) VALUES ("' +firstName+'","'+lastName+'","'+rut+'","'+email+'","'+birthday+'","'+recibe_info+'");';
+	//var sql = 'INSERT INTO registro (firstName, lastName,rut,email,birthday, recibe_info) VALUES ("eric","birkner","111111111","email@dalso.com","1212122222","si");';
+	console.log(sql);
+	tx.executeSql(sql);
+	
+		
+	
+	window.location= 'juego.html';
+	
+	
+	}, errorCB);
+	
+	
+}
+
+
+
+//funcion random array
+(function($) {
+    $.rand = function(arg) {
+        if ($.isArray(arg)) {
+            return arg[$.rand(arg.length)];
+        } else if (typeof arg === "number") {
+            return Math.floor(Math.random() * arg);
+        } else {
+            return 4;  // chosen by fair dice roll
+        }
+    };
+})(jQuery);
+
+
+
+//validacion de formulario
+
+$(document).ready(function() {
+  
+	$('form').on('submit', function(Event){
+    	// validation code here
+		Event.preventDefault();
+		var rut = $('#rut').val();
+		// retorna true si es válido
+		if($.validateRut(rut)) {
+			
+			goRegistro();
+			
+		}else{
+			alert('El rut no es válido');
+		}
+		
+		
+  	});
+});
+
---
+++ Ballantines Games/www/js/wheel.js
@@ -0,0 +1,258 @@
+// JavaScript Document
+var parlante =0;
+var parlante = 0;
+var audifono = 0;
+var finest = 0;
+
+
+
+
+var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+
+db.transaction(function(tx){
+	var sql = "SELECT * FROM premios where name like ('%parlante%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length>0){
+			console.log('parlante :'+results.rows.item(0).number);
+			hola(results.rows.item(0).number);
+		}
+
+	});
+
+});
+
+db.transaction(function(tx){
+	var sql = "SELECT * FROM premios where name like ('%audifono%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length>0){
+			console.log('audifono :'+results.rows.item(0).number);
+			hola1(results.rows.item(0).number);
+		}
+
+	});
+
+});
+
+db.transaction(function(tx){
+	var sql = "SELECT * FROM premios where name like ('%finest%') limit 1";	
+	console.log(sql);	
+	tx.executeSql(sql,
+	[], function(tx, results) {
+		console.log(results);
+		if(results.rows.length>0){
+			console.log('finest :'+results.rows.item(0).number);
+			hola2(results.rows.item(0).number);
+		}
+
+	});
+
+});
+		
+function hola(number){
+	parlante = number;
+	console.log(parlante);
+}
+
+function hola1(number){
+	audifono = number;
+	console.log(audifono);
+}
+
+function hola2(number){
+	finest = number;
+	console.log(finest);
+}
+
+
+function descontar(premio) {
+	var db = window.openDatabase(db_name, "1.0", "Birkner Media", 200000);
+	db.transaction(function(tx) {
+		var sql = 'UPDATE premios SET number= number -1 WHERE name like "%'+premio+'%"; ';
+		console.log(sql);	
+		tx.executeSql(sql);
+	}, errorCB);
+	
+}
+
+
+$(function(){
+	
+		window.WHEELOFFORTUNE = {
+
+            cache: {},
+
+            init: function () {
+                //console.log('controller init...');
+
+                var _this = this;
+                this.cache.wheel = $('.rueda');
+                this.cache.wheelMarker = $('.indicador');
+                //this.cache.wheelSpinBtn = $('.rueda, .boton-ruleta'); //im using the wheel as the spin button but simply change this to a button if you want.
+				this.cache.wheelSpinBtn = $('#juego .rueda'); //im using the wheel as the spin button but simply change this to a button if you want.
+
+                //mapping is backwards as wheel spins clockwise //1=win
+                //this.cache.wheelMapping = [400, 120, 80, 750, 150, 300, 60, 175, 500, 125, 75, 1000, 120, 200, 90, 600, 100, 250].reverse();
+				this.cache.wheelMapping = [1, 2, 3, 4, 5, 6, 7, 8].reverse();
+
+				
+                this.cache.wheelSpinBtn.on('click swipedown swiperight', function (e) {
+                    e.preventDefault();
+                    if (!$(this).hasClass('disabled')) _this.spin();
+					$('.boton-ruleta').css('visibility','hidden');
+                });
+				
+
+                //reset wheel
+                this.resetSpin();
+
+                //setup prize events
+                //this.prizeEvents();
+            },
+
+            spin: function () {
+                //console.log('spinning wheel');
+
+                var _this = this;
+
+                // reset wheel
+                this.resetSpin();
+
+                //disable spin button while in progress
+                this.cache.wheelSpinBtn.addClass('disabled');
+
+                /*
+                    Wheel has 10 sections.
+                    Each section is 360/10 = 36deg.
+                */
+                //var deg = 1500 + Math.round(Math.random() * 1500),
+                //duration = 6000; //optimal 6 secs
+				
+				//var premios = Array(45,90,135,180,225,270,315,360);
+				//var nombre_premios = Array('parlante','sigue disfrutando','parlante','audifonos','parlante','sigue disfrutando','parlante','audifonos');
+				var premios = Array(90,90,90,90,90,90,90,90);
+				var nombre_premios = Array('sigue disfrutando','sigue disfrutando','sigue disfrutando','sigue disfrutando','sigue disfrutando','sigue disfrutando','sigue disfrutando','sigue disfrutando');
+				
+				if(parlante>0){
+					console.log('hay parlante');
+					premios[0]=45;
+					premios[4]=225;
+					nombre_premios[0]="parlante";
+					nombre_premios[4]="parlante";
+				}
+				
+				if(audifono>0){
+					console.log('hay audifonos');
+					premios[2]=135;
+					premios[6]=315;
+					nombre_premios[2]="audifono";
+					nombre_premios[6]="audifono";
+				}
+				
+				if(finest>0){
+					console.log('hay finest');
+					premios[3]=180;
+					premios[7]=360;
+					nombre_premios[3]="finest";
+					nombre_premios[7]="finest";
+				}
+				
+				console.log(nombre_premios);
+				
+				var random = Math.floor(Math.random()*premios.length);
+				var premiado = premios[random];
+				
+				console.log('premiado:'+ premiado);
+				
+				var yapa = 20; //la yapa se usa para que quede marcado al medio
+				var deg = 3600 + premiado + yapa,
+                duration = 6000; //optimal 6 secs
+
+                _this.cache.wheelPos = deg;
+
+                //transition queuing
+                //ff bug with easeOutBack
+                this.cache.wheel.transition({
+                    rotate: '0deg'
+                }, 0)
+                    .transition({
+                    rotate: deg + 'deg'
+                }, duration, 'easeOutCubic');
+
+                //move marker
+                _this.cache.wheelMarker.transition({
+                    rotate: '-20deg'
+                }, 0, 'snap');
+
+                //just before wheel finish
+                setTimeout(function () {
+                    //reset marker
+                    _this.cache.wheelMarker.transition({
+                        rotate: '0deg'
+                    }, 300, 'easeOutQuad');
+                }, duration - 500);
+
+                //wheel finish
+                setTimeout(function () {
+                    // did it win??!?!?!
+                    var spin = _this.cache.wheelPos,
+                        degrees = spin % 360,
+                        percent = (degrees / 360) * 100,
+                        segment = Math.ceil((percent / 9)),  
+                        //win = _this.cache.wheelMapping[segment]; //zero based array
+						win = 1,
+						premio ="";
+
+                   	console.log('spin = ' + spin);
+                    console.log('degrees = ' + degrees);
+                    console.log('percent = ' + percent);
+                    console.log('segment = ' + segment);
+                    
+					//	
+					premio = nombre_premios[random];
+						
+					
+					console.log('win = ' + win);
+					console.log('premio = ' + premio);
+					
+					if(premio!=='sigue disfrutando'){
+						if(premio === 'audífonos'){
+							descontar('audifonos');
+						}else{
+							descontar(premio);	
+						}						
+					}
+										
+					//alert(premio);
+					$('#juego .tu_premio .box_premio').text(premio);
+					$('#juego .tu_premio').css({'display':'block'});
+					
+					//$('input[name=premio]').val(premio);
+                   
+					/*
+					$.post("save.php", $('#form').serialize(), function( data ) {
+						$('.boton-volver').fadeIn('slow');
+					});	
+					
+					*/
+                
+				}, duration);
+
+            },
+
+            resetSpin: function () {
+                this.cache.wheel.transition({
+                    rotate: '0deg'
+                }, 0);
+                this.cache.wheelPos = 0;
+            }
+
+        }
+
+        window.WHEELOFFORTUNE.init();
+});
